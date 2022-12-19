ad1bf5c457808ae270f3de312b865e76fe388e9a
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-19 14:31:50 +0000

    Add group model and it's validations

---
+++ app/models/group.rb
@@ -0,0 +1,7 @@
+class Group < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_and_belongs_to_many :accounts
+
+  validates :name, presence: true
+  validates :icon, presence: true
+end
