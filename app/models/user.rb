8a668624d3fce3bc6fc97bf8fef23434c22ef2bc
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-19 21:55:09 +0000

    Add device for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :accounts, class_name: 'Account', foreign_key: 'author_id'
   has_many :groups, class_name: 'Group', foreign_key: 'author_id'
 
