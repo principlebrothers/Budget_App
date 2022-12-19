fc7a061e42019a240d60b60bbb01363f323c9dde
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-19 15:41:30 +0000

    Add model test for all model classes

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Account.create(name: 'Savings', author_id: 1) }
+
+  it 'should validate presence of name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should belong to author' do
+    assc = described_class.reflect_on_association(:user)
+    expect(assc.macro).to eq :belongs_to
+  end
+
+  it 'should have many groups' do
+    assc = described_class.reflect_on_association(:groups)
+    expect(assc.macro).to eq :has_and_belongs_to_many
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.create(name: 'John Kuma') }
+
+  it 'should validate presence of name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have many accounts' do
+    assc = described_class.reflect_on_association(:accounts)
+    expect(assc.macro).to eq :has_many
+  end
+
+  it 'should have many groups' do
+    assc = described_class.reflect_on_association(:groups)
+    expect(assc.macro).to eq :has_many
+  end
 end
