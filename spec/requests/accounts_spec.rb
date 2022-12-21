152515d099a1eb6680a62c7daefc619afa9317da
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-21 18:24:33 +0000

    Generate test boiler plate for account

---
+++ spec/requests/accounts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Accounts", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/accounts/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/accounts/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
