aff441a5e06ee7061aef07c90ab99457616ca541
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-21 19:34:59 +0000

    Update pages_spec.rb

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,8 +3,23 @@ require 'rails_helper'
 RSpec.describe "Pages", type: :request do
   describe "GET /home" do
     it "returns http success" do
-      get "/pages/home"
+      get root_path
       expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get root_path
+      expect(response.body).to include("BudgetBuddy")
+    end
+
+    it 'should have a link to sign up' do
+      get root_path
+      expect(response.body).to include("Sign up")
+    end
+
+    it 'should have a link to Log in' do
+      get root_path
+      expect(response.body).to include("Log in")
     end
   end
 
