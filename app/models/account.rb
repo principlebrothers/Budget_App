ccd2ec6b7b24f7231d8e1124d9479f993f0ef95c
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-21 18:20:02 +0000

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 class Account < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  has_and_belongs_to_many :groups
+  has_many :group_accounts
+  has_many :groups, through: :group_accounts
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 1_000_000_000_000, message: "must be a valid amount"}
