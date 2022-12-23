5b6d5b772971a7b4b84334ee939b0ec4c33cfc17
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-23 15:04:21 +0000

    Configure production mailing for app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,25 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Budget_App_production"
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:     'adonuernest@gmail.com',
+    password:      'lsznfajdzcvuwcck',
+    domain:        'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
+
+  config.action_mailer.default_url_options = { host: 'budget-app-xk5s.onrender.com', port: 443 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
