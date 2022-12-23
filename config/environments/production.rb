f413704cfca0c1e661a1e108d58e998f57acfd16
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2022-12-23 15:27:40 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:     'adonuernest@gmail.com',
     password:      'lsznfajdzcvuwcck',
-    domain:        'localhost:3000',
+    domain:        'budget-app-xk5s.onrender.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
